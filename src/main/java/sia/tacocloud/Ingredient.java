00455b96fdc487b3d1a615c84da0fc2bb2710a56
Andrii <64665760+hiddenns@users.noreply.github.com>
2022-06-04 23:34:09 +0300

    chapter 2

---
+++ src/main/java/sia/tacocloud/Ingredient.java
@@ -0,0 +1,18 @@
+package sia.tacocloud;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Data
+@RequiredArgsConstructor
+public class Ingredient {
+
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public static enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
